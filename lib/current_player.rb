3d05bc8f2bce6dcdf3d9e954e96237cd0ebf660b
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-13 17:41:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,5 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   counter = 0
-  board.count do |turn_count|
-  turn_count.to_i
-  counter += 1
-  end
+  board.count
 end
